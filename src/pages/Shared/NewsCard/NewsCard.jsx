a7aaf2f5219e815cf85a7c8a2ca8de9a26494532
mirzaistiakhislam <mirza15-9603@diu.edu.bd>
2023-08-16 15:07:18 +0600

    add react rating and total view in the card footer

--- src/pages/Shared/NewsCard/NewsCard.jsx
+++ src/pages/Shared/NewsCard/NewsCard.jsx
@@ -2,15 +2,17 @@ import moment from 'moment';
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import { FaRegBookmark, FaShareNodes } from "react-icons/fa6";
+import { FaRegBookmark, FaShareNodes, FaEye } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
+import '@smastrom/react-rating/style.css';
+import { Rating } from '@smastrom/react-rating';
 
 const NewsCard = ({ news }) => {
 
     const { author: { img, name, published_date }, details, image_url, rating: { number }, title, total_view, _id } = news;
 
     return (
-        <Card>
+        <Card className='mb-3'>
             <Card.Header>
                 <div className='d-flex align-items-center'>
                     <div>
@@ -21,7 +23,7 @@ const NewsCard = ({ news }) => {
                         <span className=''>{moment().format("YYYY-MM-DD")}</span>
                     </div>
                     <div>
-                        <FaRegBookmark />
+                        <FaRegBookmark className='me-2' />
                         <FaShareNodes />
                     </div>
                 </div>
@@ -40,7 +42,22 @@ const NewsCard = ({ news }) => {
                 </Card.Text>
                 <Button variant="primary">Go somewhere</Button>
             </Card.Body>
-            <Card.Footer className="text-muted">2 days ago</Card.Footer>
+            <Card.Footer className="text-muted">
+                <div className='d-flex align-items-center justify-content-between'>
+                    <div className='d-flex'>
+                        <Rating
+                            style={{ maxWidth: 100 }}
+                            value={number}
+                            readOnly
+                        />
+                        <span className='ms-1'>{number}</span>
+                    </div>
+                    <div className='d-flex align-items-center'>
+                        <FaEye />
+                        <span className='ms-2'>{total_view}</span>
+                    </div>
+                </div>
+            </Card.Footer>
         </Card>
     );
 };
